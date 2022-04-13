5a9cf14511fdd17070b46b353e463f165d233c71
Blue-Bug <kjj9987@gmail.com>
2022-04-14 02:08:56 +0900

    Auth api v1.0.2

--- Readme.md
+++ Readme.md
@@ -4,6 +4,7 @@
 ## Apis 
 ### gamehub_auth_api
 #### Member 인증/인가를 담당하는 실행가능한 api 모듈
+v1.0.2 cors 설정
 ## Domains 
 ### token_domain
 #### token 도메인 모듈
--- apis/gamehub_auth_api/src/main/java/com/bluebug/gamehub/gamehub_auth_api/config/SecurityConfig.java
+++ apis/gamehub_auth_api/src/main/java/com/bluebug/gamehub/gamehub_auth_api/config/SecurityConfig.java
@@ -4,12 +4,18 @@ import com.bluebug.gamehub.gamehub_core.jwt.JwtAccessDeniedHandler;
 import com.bluebug.gamehub.gamehub_core.jwt.JwtAuthenticationEntryPoint;
 import com.bluebug.gamehub.gamehub_core.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @Configuration
@@ -38,5 +44,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .and()
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
